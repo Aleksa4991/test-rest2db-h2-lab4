49a6b95c6b36c61ecd7a299ccbb4a41921d1268b
Александра <aleksa4991@mail.ru>
2022-10-11 16:54:30 +0500

    DateBase students

---
+++ src/main/java/com/example/testrest2dbh2lab4/TestRest2dbH2Lab4Application.java
@@ -0,0 +1,13 @@
+package com.example.testrest2dbh2lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRest2dbH2Lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRest2dbH2Lab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testrest2dbh2lab4/TestRest2dbH2Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrest2dbh2lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRest2dbH2Lab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
